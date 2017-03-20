880a9437e06f69bf396ced69bcf23d806d9f491c
Mayer Dmitriy Yakovlevich <majer-dy@fxclub.org>
2017-03-20 17:27:08 +0100

    escaped path

--- src/org/fxclub/qa/jenkins/Cucumber.groovy
+++ src/org/fxclub/qa/jenkins/Cucumber.groovy
@@ -16,8 +16,8 @@ class Cucumber implements Serializable {
 
     List<JsonFeature> parseCucumberJsonReport(path){
         def workspace = steps.pwd tmp: true
-        steps.echo "Parsing JSON file: ${workspace}/${path}"
-        def jsonString = steps.readJSON file: "${workspace}/${path}"
+        steps.echo "Parsing JSON file: '${workspace}/${path}'"
+        def jsonString = steps.readJSON file: "'${workspace}/${path}'"
         ObjectMapper mapper = new ObjectMapper()
         List<JsonFeature> json = mapper.readValue(
                 jsonString,
