c3348478168bedbb9c7d1a27454e437847fb8f05
Mayer Dmitriy Yakovlevich <majer-dy@fxclub.org>
2017-03-21 15:50:41 +0100

    added equals

--- src/org/fxclub/qa/jenkins/internal/JsonFeature.groovy
+++ src/org/fxclub/qa/jenkins/internal/JsonFeature.groovy
@@ -33,13 +33,13 @@ class JsonFeature extends JsonBase {
 
         JsonFeature that = (JsonFeature) o
 
-        if (comments != that.comments) return false
+        if (!comments.equals(that.comments)) return false
         if (description != that.description) return false
         if (id != that.id) return false
         if (keyword != that.keyword) return false
         if (line != that.line) return false
         if (name != that.name) return false
-        if (tags != that.tags) return false
+        if (!tags.equals(that.tags)) return false
         if (uri != that.uri) return false
 
         return true
