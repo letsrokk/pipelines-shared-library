977a2c9f395056f65583f22f90f78975206ba152
Mayer Dmitriy Yakovlevich <majer-dy@fxclub.org>
2017-12-22 09:38:52 +0100

    debugging for testng.groovy

--- Script.groovy
+++ Script.groovy
@@ -1,15 +1,5 @@
-package org.fxclub.qa.jenkins
+import org.fxclub.qa.jenkins.TestNG
 
-import org.fxclub.qa.jenkins.internal.cucumber.JsonFeature
-Cucumber cucumber = new Cucumber()
+def testNG = new TestNG();
 
-List<JsonFeature> mergedReport = new ArrayList<>()
-def reportsDir = new File('/Users/majer-dy/Documents/IDEA/registration-services/target/cucumber-parallel')
-reportsDir.eachFileRecurse(groovy.io.FileType.FILES) {
-    if(it.name.endsWith('.json')) {
-        List<JsonFeature> features = cucumber.parseCucumberJsonReport(it.getAbsolutePath())
-        mergedReport = cucumber.mergeReport(mergedReport, features)
-    }
-}
-
-cucumber.writeReport(mergedReport)
+TestNG.mergeSuites("/Users/majer-dy/Documents/IDEA/web-testing-framework", "fxbank", "news;audit;", "", "")
