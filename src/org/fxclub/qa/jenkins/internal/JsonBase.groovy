7c8fb10f993e8998b9ee1c42d9875c4eed1bae0e
Mayer Dmitriy Yakovlevich <majer-dy@fxclub.org>
2017-12-26 14:59:40 +0700

    removed @Grabs

--- src/org/fxclub/qa/jenkins/internal/JsonBase.groovy
+++ src/org/fxclub/qa/jenkins/internal/JsonBase.groovy
@@ -1,8 +1,5 @@
 package org.fxclub.qa.jenkins.internal
 
-@Grapes(
-        @Grab(group='com.fasterxml.jackson.core', module='jackson-annotations', version='2.9.0.pr1')
-)
 import com.fasterxml.jackson.annotation.JsonInclude
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
