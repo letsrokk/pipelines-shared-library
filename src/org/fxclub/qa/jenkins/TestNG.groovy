0a9baa42e0ba7a9c1191f9c64d1f617327ddaef0
Mayer Dmitriy Yakovlevich <majer-dy@fxclub.org>
2017-12-27 11:50:23 +0700

    fixed error

--- src/org/fxclub/qa/jenkins/TestNG.groovy
+++ src/org/fxclub/qa/jenkins/TestNG.groovy
@@ -2,6 +2,7 @@ package org.fxclub.qa.jenkins
 
 import org.apache.commons.io.IOUtils
 import org.apache.commons.lang3.StringUtils
+import org.w3c.dom.Attr
 import org.w3c.dom.Document;
 
 class TestNG implements Serializable {
