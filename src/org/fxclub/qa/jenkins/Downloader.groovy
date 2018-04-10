a4d4945cbd99d9ed51f780e1b650d3bc514f1145
Dmitry Mayer <dmitry.mayer@fxclub.com>
2018-04-10 16:49:48 +0200

    switch file filter for pipe step

--- src/org/fxclub/qa/jenkins/Downloader.groovy
+++ src/org/fxclub/qa/jenkins/Downloader.groovy
@@ -10,25 +10,12 @@ class Downloader {
         this.steps = steps
     }
 
-    List<File> downloadJenkinsArtifacts(String buildUrl, def extensions=[], def user=null, def passwd=null){
+    List<String> downloadJenkinsArtifacts(String buildUrl, def extensions=[], def user=null, def passwd=null){
         steps.fileOperations([
                 steps.fileDownloadOperation(password: '', targetFileName: 'zipBuilds.zip', targetLocation: '', url: "${buildUrl}artifact/*zip*/archive.zip", userName: ''),
                 steps.fileUnZipOperation(filePath: 'zipBuilds.zip', targetLocation: 'unzipBuilds')
         ])
 
-        def list = []
-        def dir = new File(unzipFolder)
-        dir.eachFileRecurse (FileType.FILES) { file ->
-            if((extensions.size > 0)) {
-                def match = extensions.any{file.getAbsolutePath().endsWith(it)}
-                if(match) {
-                    list << file
-                }
-            } else {
-                list << file
-            }
-        }
-
-        return list
+        steps.findFiles glob: 'unzipBuilds/*.zip'
     }
 }
