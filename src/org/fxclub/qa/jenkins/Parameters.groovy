4829790e0831bf090af34f186a36e7f2455bfd70
Dmitry Mayer <dmitry.mayer@fxclub.com>
2018-05-25 14:47:19 +0200

    debug

--- src/org/fxclub/qa/jenkins/Parameters.groovy
+++ src/org/fxclub/qa/jenkins/Parameters.groovy
@@ -2,17 +2,10 @@ package org.fxclub.qa.jenkins
 
 class Parameters implements Serializable {
 
-    def steps
-
-    Parameters(steps){
-        this.steps = steps
-    }
-
-    String getParam(param, defaultValue){
+    static String getParam(param, defaultValue){
         try{
             return "${param}"
         } catch(ignore){
-            steps.echo ignore.getMessage()
             return defaultValue
         }
     }
